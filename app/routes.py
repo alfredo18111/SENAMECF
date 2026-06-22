45ea2862875d1139ec0ecff87fbe85b845e414ce
Gdiaz9281 <Gdiaz9281@gmail.com>
2026-06-21 20:19:00 -0400

    Agregar eliminacion de empleados

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from app import db
 from app.models import Empleado, Especializacion
 
 main = Blueprint("main", __name__)
@@ -27,6 +28,28 @@ def especializaciones():
         especializaciones=especializaciones
     )
 
-@main.route("/prueba")
-def prueba():
-    return "FUNCIONA"
+@main.route("/empleados/nuevo", methods=["POST"])
+def nuevo_empleado():
+
+    empleado = Empleado(
+        cedula=request.form["cedula"],
+        nombres=request.form["nombres"],
+        apellidos=request.form["apellidos"],
+        telefono=request.form["telefono"],
+        correo=request.form["correo"]
+    )
+
+    db.session.add(empleado)
+    db.session.commit()
+
+    return redirect(url_for("main.empleados"))
+
+@main.route("/empleados/eliminar/<int:id>")
+def eliminar_empleado(id):
+
+    empleado = Empleado.query.get_or_404(id)
+
+    db.session.delete(empleado)
+    db.session.commit()
+
+    return redirect(url_for("main.empleados"))
